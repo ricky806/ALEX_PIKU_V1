0d3cb2306a98b83c41ee1af6d841f48339bcd469
Rudraraj3 <147349281+Rudraraj3@users.noreply.github.com>
2024-06-14 21:05:32 +0530

    Config.js

--- Config.js
+++ Config.js
@@ -25,12 +25,12 @@ global.link = ''
 global.prefa = ['.']
 
 //false=disable and true=enable
-global.welcome = false //auto welcome
-global.autoRecording = false //auto recording
+global.welcome = true //auto welcome
+global.autoRecording = true //auto recording
 global.autoTyping = false //auto typing
 global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
-global.autobio = false //auto update bio
+global.autobio = true //auto update bio
 global.anti212 = true //auto block +212
 global.autoread_status = false //auto view status/story
 
