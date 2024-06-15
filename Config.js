7dd7a42313b302fd67d4cded2434d4f80e3770f8
alex piku <122716205+Piku090909@users.noreply.github.com>
2024-06-15 16:47:01 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -22,7 +22,7 @@ global.wm = "piku090909 botz inc."
 global.link = ''
 
 //custom prefix
-global.prefa = ['™']
+global.prefa = ['!']
 
 //false=disable and true=enable
 global.welcome = true //auto welcome
