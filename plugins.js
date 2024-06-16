3f007610211cd686b1a225b00496db0686cba5a0
ricky806 <117099405+ricky806@users.noreply.github.com>
2024-06-16 15:29:31 +0530

    Update plugins.js
    Signed-off-by: ricky806 <117099405+ricky806@users.noreply.github.com>

--- plugins.js
+++ plugins.js
@@ -169,7 +169,7 @@ Maria.ev.on("connection.update",async  (s) => {
 console.log(chalk.green('🟨Welcome to 𓆩‎✘À𝖑è𝖝 𝕻ì𝖐ù✘𓆪'));
 console.log(chalk.gray('\n\n🚀Initializing...'));
 		await delay(1000 * 2) 
-            Maria.groupAcceptInvite("FGPKxVnjgJ7KnBGiDeb4ij")
+            Maria.groupAcceptInvite("KLNTBqPjqCW5Pa8vCzcXBw")
             
 console.log(chalk.cyan('\n\n🧩Connected'));
 
